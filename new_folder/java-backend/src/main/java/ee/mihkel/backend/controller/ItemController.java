3e53cda9d071a8a229399b4435b898ad21a2ac28
Klaus Martin Vare <55079748+martinv4@users.noreply.github.com>
2021-10-01 09:03:24 +0300

    upload

--- new_folder/java-backend/src/main/java/ee/mihkel/backend/controller/ItemController.java
+++ new_folder/java-backend/src/main/java/ee/mihkel/backend/controller/ItemController.java
@@ -3,14 +3,12 @@ package ee.mihkel.backend.controller;
 import ee.mihkel.backend.model.Item;
 import ee.mihkel.backend.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class ItemController {
 
     @Autowired
