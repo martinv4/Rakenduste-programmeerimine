f955fc69429ee28098caf7b6cbec9e2be66ad12a
Klaus Martin Vare <55079748+martinv4@users.noreply.github.com>
2021-10-01 08:58:21 +0300

    upload

--- new_folder/java-backend/src/main/java/ee/mihkel/backend/controller/CategoryController.java
+++ new_folder/java-backend/src/main/java/ee/mihkel/backend/controller/CategoryController.java
@@ -3,13 +3,12 @@ package ee.mihkel.backend.controller;
 import ee.mihkel.backend.model.Category;
 import ee.mihkel.backend.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class CategoryController {
     @Autowired
     CategoryService categoryService;
